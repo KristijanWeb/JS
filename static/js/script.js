390cae2a4fec79f42dc576afdfa0a87121581ebd
Kristijan Marijić <kikristijan@gmail.com>
2020-02-24 22:23:10 +0100

    New Challenge

--- static/js/script.js
+++ static/js/script.js
@@ -1,6 +1,6 @@
 // ********************* Challenge 1 ********************
 function ageInDays(){
-    var birthYear = prompt('What year were you born... Good friend?');
+    var birthYear = prompt('What year were you born');
     var ageInDayss = (2020 - birthYear) * 365;
     var h1 = document.createElement('h1');
     var textAnswer = document.createTextNode('You are ' + ageInDayss + ' days old.');
